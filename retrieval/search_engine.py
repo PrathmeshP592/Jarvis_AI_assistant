0f533ce4fcfe12246b1f7e99fef746c8f2de35a8
PrathmeshP592 <pawarprathmeshr@gmail.com>
2026-02-17 13:03:11 +0530

    Updates

--- retrieval/search_engine.py
+++ retrieval/search_engine.py
@@ -1,10 +1,3 @@
-# from duckduckgo_search import DDGS
-
-# def search_web(query, max_results=3):
-#     with DDGS() as ddgs:
-#         results = list(ddgs.text(query, max_results=max_results))
-#     return [r["href"] for r in results]
-
 from ddgs import DDGS
 
 def search_web(query, max_results=3):
